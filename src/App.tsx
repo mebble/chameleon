f2f1b975a1f24d299253f66ce128855bd7ebae31
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-07-03 18:02:29 +0530

    [poc] highlight parts of the text based on the selection

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,63 @@
 import type { Component } from 'solid-js';
+import { createSignal, For } from 'solid-js';
+import { selectionIndexes } from './selection';
+
+const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
 
 type Character = {
-    text: string,
-    group?: string,
+    text: string;
+    group?: string;
 };
-
-const characters: Character[] = [
-    { text: 't' },
-    { text: 'h' },
-    { text: 'i' },
-    { text: 's' },
-    { text: ' ' },
-    { text: 'i', group: 'abc' },
-    { text: 's', group: 'abc' },
-    { text: ' ', group: 'abc' },
-    { text: 'a', group: 'abc' },
-    { text: 't', group: 'abc' },
-    { text: 'l', group: 'def' },
-    { text: 'a', group: 'def' },
-    { text: 's', group: 'def' },
-    { text: ' ' },
-    { text: 'o' },
-    { text: 'h' },
-];
-
 type GroupMap = Record<string, any>;
+type Text = {
+    characters: Character[];
+    groups: GroupMap;
+}
 
-const groups: GroupMap = {
-    'abc': { background: '#a5a5ff' },
-    'def': { background: '#ffd054' },
-};
-
-const CharactersDisplay: Component<{ chars: Character[], groups: GroupMap }> = (props) => {
-    const characters = props.chars;
-    const parts = partitionBy((x) => x.group, characters)
-
-    return <div class="text-2xl">
-        {parts.map(part => {
-            const style = groups[part[0].group] || {};
+const CharactersDisplay: Component<{ text: Text, ref: any }> = (props) => {
+    return <div class="text-2xl" ref={props.ref}>
+        <For each={partitionBy(x => x.group, props.text.characters)}>{(part) => {
+            const style = props.text.groups[part[0].group] || {};
             return <span style={style}>{part.map(char => char.text).join('')}</span>
-        })}
+        }}</For>
     </div>;
 };
 
 const App: Component = () => {
+    const [color, setColor] = createSignal('#f6b73c');
+    const [text, setText] = createSignal<Text>({ characters: generate(lorem), groups: {} });
+    let ref;
+
+    const handleHighlight = _event => {
+        const selection = document.getSelection();
+        const [start, end] = selectionIndexes(selection, ref)
+        
+        const newGroupId = randomStr();
+        const t = text();
+        const characters = t.characters
+        const newChars = [
+            ...characters.slice(0, start),
+            ...characters.slice(start, end).map(c => ({ ...c, group: newGroupId })),
+            ...characters.slice(end)
+        ];
+        setText({
+            characters: newChars,
+            groups: {
+                ...t.groups,
+                [newGroupId]: { background: color() }
+            }
+        })
+    };
+
     return <div class="text-center py-20">
-        <p class="text-4xl mb-2">
-            <button>Hello chameleon!</button>
-        </p>
-        <CharactersDisplay chars={characters} groups={groups} />
+        <header class="text-4xl mb-2">
+            <h1>Hello chameleon!</h1>
+        </header>
+        <CharactersDisplay text={text()} ref={ref} />
+        <div class="inline-flex mt-2 gap-3">
+            <input type="color" value={color()} onInput={e => setColor(e.target.value)} />
+            <button onClick={handleHighlight} class="bg-gray-500 text-neutral-100 rounded px-1">Highlight</button>
+        </div>
     </div>;
 };
 
@@ -68,6 +77,14 @@ function partitionBy<T> (accessor: (x: T) => any, arr: T[]): T[][] {
         }
     }
     return partitions;
+}
+
+function generate(text: string): Character[] {
+    return text.split('').map(c => ({ text: c }));
+}
+
+function randomStr(): string {
+    return Math.random().toString(36).substring(2, 5);
 }
 
 export default App;
